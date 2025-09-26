81b1f90d27b191aba337afc44ed0b767464287af
jpbascon <jpbascon50@gmail.com>
2025-09-26 23:10:57 +0800

    hope this works part 6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,19 +6,20 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+      //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(){
+    if (env('APP_ENV') === 'production') {
+      URL::forceScheme('https');
     }
+  }
 }
